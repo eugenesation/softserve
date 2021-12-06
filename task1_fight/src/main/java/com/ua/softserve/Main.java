cb1a3287d2cab65182c75d8dc1a944e72076f282
eugenesation <eugenesmirnov03@gmail.com>
2021-12-06 04:55:12 +0200

    Defender class has added.

--- task1_fight/src/main/java/com/ua/softserve/Main.java
+++ task1_fight/src/main/java/com/ua/softserve/Main.java
@@ -1,15 +1,15 @@
 package com.ua.softserve;
 
+import com.ua.softserve.entity.Defender;
 import com.ua.softserve.entity.Knight;
-import com.ua.softserve.entity.Warrior;
 import com.ua.softserve.service.Battle;
 
 public class Main {
 
     public static void main(String[] args) {
-        var warrior = new Warrior();
-        var knight = new Knight();
-        System.out.println(Battle.fight(warrior, knight));
+          var defender2 = new Defender();
+          var knight = new Knight();
+        System.out.println(Battle.fight(defender2, knight));
     }
 
 }
--- task1_fight/src/main/java/com/ua/softserve/entity/Defender.java
+++ task1_fight/src/main/java/com/ua/softserve/entity/Defender.java
@@ -5,4 +5,27 @@ public class Defender extends Warrior {
     private static int attack = 3;
     private static int defence = 2;
 
+    @Override
+    public int getHealth() {
+        return health;
+    }
+
+    @Override
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    @Override
+    public int getAttack() {
+        return attack;
+    }
+
+    public static int getDefence() {
+        return defence;
+    }
+
+    @Override
+    public void setDamage(Warrior warrior) {
+        setHealth(getHealth() - warrior.getAttack() + getDefence());
+    }
 }
