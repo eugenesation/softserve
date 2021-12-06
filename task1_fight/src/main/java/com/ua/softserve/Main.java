0c5519d98e25103cffc5b93e92942a0c3fa09e88
eugenesation <eugenesmirnov03@gmail.com>
2021-12-06 22:52:07 +0200

    Defender class has added.

--- task1_fight/src/main/java/com/ua/softserve/Main.java
+++ task1_fight/src/main/java/com/ua/softserve/Main.java
@@ -2,6 +2,7 @@ package com.ua.softserve;
 
 import com.ua.softserve.entity.Defender;
 import com.ua.softserve.entity.Knight;
+import com.ua.softserve.entity.Warrior;
 import com.ua.softserve.service.Battle;
 
 public class Main {
@@ -9,7 +10,8 @@ public class Main {
     public static void main(String[] args) {
           var defender2 = new Defender();
           var knight = new Knight();
-        System.out.println(Battle.fight(defender2, knight));
+          var warrior = new Warrior();
+        System.out.println(Battle.fight(warrior, defender2));
     }
 
 }
